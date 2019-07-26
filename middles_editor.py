3704d8da1e0f96364ec54997d9dbdd3f2704dde2
beaudrychase <beaudrychase@utexas.edu>
2019-07-26 00:50:39 -0500

    Changed some settings. Now the top three buttons work the way they should

--- middles_editor.py
+++ middles_editor.py
@@ -17,12 +17,13 @@ class AppWindow(QMainWindow):
         self.read_entry_data(self.entry_index)
         # self.ui.is_middle_button.clicked.connect(self.status_label.update)
         # self.ui.is_not_middle_button.clicked.connect(self.status_label.update)
-        #self.ui.reset_entry_button.clicked.connect(self.status_label.update)
+        # self.ui.reset_entry_button.clicked.connect(self.status_label.update)
 
         self.ui.next_button.clicked.connect(self.next_entry)
         self.ui.previous_button.clicked.connect(self.previous_entry)
         self.show()
 
+    # Creates the initial state for the output json and fills in the fields that need to be initialized.
     def create_output_json(self, input_file_name):
         output_json = {}
         # the time this program was run
@@ -37,10 +38,30 @@ class AppWindow(QMainWindow):
         output_json["sentences_status"] = [False] * len(self.input["results"])
         return output_json
 
+    # Prepares the form for a new middle by enabling and disabling things. Doesn't display the middle
+    def prepare_for_new_entry(self):
+        self.ui.is_middle_button.setEnabled(True)
+        self.ui.is_not_middle_button.setEnabled(True)
+
+        #all of the widgets for editing the middle.
+        self.ui.reset_entry_button.setEnabled(False)
+        self.ui.note_editor.setEnabled(False)
+        self.ui.which_middle_spin_box.setEnabled(False)
+        self.ui.add_middle_button.setEnabled(False)
+        self.ui.remove_middle_button.setEnabled(False)
+        self.ui.subject_line_edit.setEnabled(False)
+        self.ui.subject_spin_box.setEnabled(False)
+        self.ui.verb_line_edit.setEnabled(False)
+        self.ui.verb_spin_box.setEnabled(False)
+        self.ui.adverb_line_edit.setEnabled(False)
+        self.ui.adverb_spin_box.setEnabled(False)
+
+    # Displays a entry.
     def read_entry_data(self, index):
         # I want this to set all of the fields to show the data.
+        #TODO make this read in the values from the input file.
         entry = self.input['results'][index]
-        self.ui.groupBox.setTitle("Middle {0}/{1}".format(index, self.input["sentence_number"]))
+        self.ui.groupBox.setTitle("Middle {0}/{1}".format(index, self.input["sentence_number"] - 1))
         self.ui.note_editor.setPlainText(entry["middles"][0]["note"])
         self.ui.status_label.setText("unaccounted for")
         self.ui.sentence_label.setText(entry['sentence'])
@@ -67,13 +88,7 @@ class AppWindow(QMainWindow):
             self.read_entry_data(self.entry_index)
         return True
 
-    def prepare_for_new_entry(self):
-        self.ui.is_middle_button.setEnabled(True)
-        self.ui.is_not_middle_button.setEnabled(True)
-        self.ui.reset_entry_button.setEnabled(False)
-        self.ui.note_editor.setEnabled(False)
-        self.ui.which_middle_spin_box.setEnabled(False)
-        self.ui.add_middle_button.setEnabled(False)
+
 
 
 app = QApplication(sys.argv)
