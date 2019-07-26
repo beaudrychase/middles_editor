cc5a5257f2cf372b7323b2ee18fbece231517167
beaudrychase <beaudrychase@utexas.edu>
2019-07-25 23:36:41 -0500

    It can display a single middle read in from the file. progress

--- middles_editor.py
+++ middles_editor.py
@@ -11,7 +11,20 @@ class AppWindow(QMainWindow):
         self.ui.setupUi(self)
         with open("test.json", 'r') as f:
             self.input = json.load(f)
+        self.read_entry_data(0)
         self.show()
+
+    def read_entry_data(self, index):
+        # I want this to set all of the fields to show the data.
+        entry = self.input['results'][index]
+        self.ui.groupBox.setTitle("Middle {0}/{1}".format(index, self.input["sentence_number"]))
+        self.ui.sentence_label.setText(entry['sentence'])
+        self.ui.subject_line_edit.setText(entry["middles"][0]["subject"]["word"])
+        self.ui.subject_spin_box.setValue(entry["middles"][0]["subject"]["index"])
+        self.ui.verb_line_edit.setText(entry["middles"][0]["verb"]["word"])
+        self.ui.verb_spin_box.setValue(entry["middles"][0]["verb"]["index"])
+        self.ui.adverb_line_edit.setText(entry["middles"][0]["adverb"]["word"])
+        self.ui.adverb_spin_box.setValue(entry["middles"][0]["adverb"]["index"])
 
 
 app = QApplication(sys.argv)
