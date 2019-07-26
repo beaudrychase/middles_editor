b00bfcf3466d1b7056dcf97852ee70077f24e7a6
beaudrychase <beaudrychase@utexas.edu>
2019-07-26 14:07:57 -0500

    I think everything is in place to save the files.

--- middles_editor.py
+++ middles_editor.py
@@ -17,7 +17,7 @@ class AppWindow(QMainWindow):
         self.output = self.create_output_json("test.json")
         self.entry_index = 0
         self.entry_result = self.output["data_results"][self.entry_index]
-        self.read_entry_data()
+        self.change_entries(0)
         self.ui.is_middle_button.clicked.connect(self.save_middle)
         # self.ui.is_not_middle_button.clicked.connect(self.status_label.update)
         # self.ui.reset_entry_button.clicked.connect(self.status_label.update)
@@ -48,25 +48,6 @@ class AppWindow(QMainWindow):
         # start it off as the entry from the input file. notes will be added and such
         self.entry_result = self.input["results"][self.entry_index]
 
-    # Prepares the form for a new middle by enabling and disabling things. Doesn't display the middle
-    def prepare_for_new_entry(self):
-        self.entry_result = self.output["data_results"][self.entry_index]
-        self.ui.is_middle_button.setEnabled(True)
-        self.ui.is_not_middle_button.setEnabled(True)
-
-        # all of the widgets for editing the middle.
-        self.ui.reset_entry_button.setEnabled(False)
-        self.ui.note_editor.setEnabled(False)
-        self.ui.which_middle_spin_box.setEnabled(False)
-        self.ui.add_middle_button.setEnabled(False)
-        self.ui.remove_middle_button.setEnabled(False)
-        self.ui.subject_line_edit.setEnabled(False)
-        self.ui.subject_spin_box.setEnabled(False)
-        self.ui.verb_line_edit.setEnabled(False)
-        self.ui.verb_spin_box.setEnabled(False)
-        self.ui.adverb_line_edit.setEnabled(False)
-        self.ui.adverb_spin_box.setEnabled(False)
-
     # Displays a entry.
     def read_entry_data(self):
         # I want this to set all of the fields to show the data.
@@ -87,7 +68,7 @@ class AppWindow(QMainWindow):
         self.ui.which_middle_spin_box.setValue(0)
         self.ui.groupBox.setTitle("Middle {0}/{1}".format(self.entry_index, self.input["sentence_number"] - 1))
         self.ui.note_editor.setPlainText(entry["middles"][0]["note"])
-        self.ui.sentence_label.setText(entry['sentence'])
+        self.ui.sentence_label.setText(entry["sentence"])
         self.ui.subject_line_edit.setText(entry["middles"][0]["subject"]["word"])
         self.ui.subject_spin_box.setValue(entry["middles"][0]["subject"]["index"])
         self.ui.verb_line_edit.setText(entry["middles"][0]["verb"]["word"])
@@ -96,26 +77,14 @@ class AppWindow(QMainWindow):
         self.ui.adverb_spin_box.setValue(entry["middles"][0]["adverb"]["index"])
 
     def next_entry(self):
-        if self.entry_index < len(self.input["results"]) - 1:
-            self.record_current_entry()
-            self.entry_index += 1
-            # TODO save entry
-            self.prepare_for_new_entry()
-            self.read_entry_data()
-        return True
+        self.change_entries(self.entry_index + 1)
 
     def previous_entry(self):
-        if self.entry_index > 0:
-            self.record_current_entry()
-            self.entry_index -= 1
-            # TODO save entry
-            self.prepare_for_new_entry()
-            self.read_entry_data()
-        return True
+        self.change_entries(self.entry_index - 1)
 
-    # called when changing entries. Saves the current data to the output JSON
-    def record_current_entry(self):
-        # if it is being saved, we need to save data.
+    def change_entries(self, new_index):
+        # make sure any changes made to the entry are saved to the output file
+        # if the entry needs to be saved
         if self.entry_result is not None and self.entry_result is not False:
             # gather data stored in widgets into the result
             middle = self.entry_result["middles"][self.ui.which_middle_spin_box.value()]
@@ -127,8 +96,59 @@ class AppWindow(QMainWindow):
             middle["adverb"]["word"] = self.ui.adverb_line_edit.text()
             middle["adverb"]["index"] = self.ui.adverb_spin_box.value()
             self.entry_result["middles"][self.ui.which_middle_spin_box.value()] = middle
-
+        # save the updated entry to the output json
         self.output["data_results"][self.entry_index] = self.entry_result
+        # change data to point to new entry
+        # check extreme values
+        if new_index < 0:
+            self.entry_index = 0
+        elif new_index > len(self.input["results"]) - 1:
+            self.entry_index = len(self.input["results"]) - 1
+        else:
+            self.entry_index = new_index
+        # load in the status of the new entry
+        self.entry_result = self.output["data_results"][self.entry_index]
+        # there are three cases the widgets need to be aware of.
+        if self.entry_result is None or self.entry_index is False:
+            # all of the widgets for editing the middle.
+            # we don't want them enabled
+            self.ui.note_editor.setEnabled(False)
+            self.ui.which_middle_spin_box.setEnabled(False)
+            self.ui.which_middle_spin_box.setMaximum(0)
+            self.ui.add_middle_button.setEnabled(False)
+            self.ui.remove_middle_button.setEnabled(False)
+            self.ui.subject_line_edit.setEnabled(False)
+            self.ui.subject_spin_box.setEnabled(False)
+            self.ui.verb_line_edit.setEnabled(False)
+            self.ui.verb_spin_box.setEnabled(False)
+            self.ui.adverb_line_edit.setEnabled(False)
+            self.ui.adverb_spin_box.setEnabled(False)
+        if self.entry_result is None:
+            self.ui.is_not_middle_button.setEnabled(True)
+            self.ui.is_middle_button.setEnabled(True)
+            self.ui.reset_entry_button.setEnabled(False)
+        elif self.entry_result is False:
+            self.ui.is_not_middle_button.setEnabled(True)
+            self.ui.is_middle_button.setEnabled(False)
+            self.ui.reset_entry_button.setEnabled(True)
+        else:
+            # the middle is saved and all of the editing widgets need to be enabled
+            self.ui.is_not_middle_button.setEnabled(False)
+            self.ui.is_middle_button.setEnabled(True)
+            self.ui.reset_entry_button.setEnabled(True)
+            self.ui.note_editor.setEnabled(True)
+            self.ui.which_middle_spin_box.setEnabled(True)
+            self.ui.which_middle_spin_box.setMaximum(len(self.entry_result["middles"]) - 1)
+            self.ui.add_middle_button.setEnabled(True)
+            self.ui.remove_middle_button.setEnabled(True)
+            self.ui.subject_line_edit.setEnabled(True)
+            self.ui.subject_spin_box.setEnabled(True)
+            self.ui.verb_line_edit.setEnabled(True)
+            self.ui.verb_spin_box.setEnabled(True)
+            self.ui.adverb_line_edit.setEnabled(True)
+            self.ui.adverb_spin_box.setEnabled(True)
+        # load the data into the widgets
+        self.read_entry_data()
 
 
 app = QApplication(sys.argv)
